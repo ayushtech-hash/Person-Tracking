d5efdf2a4de8fccf694b112f3e7cbb24d1236b03
yash-odoo <yash.tis420@gmail.com>
2026-08-04 16:41:16 +0530

    update, processor.py, visualizer.py

--- src/processor.py
+++ src/processor.py
@@ -1,14 +1,17 @@
+from src import video_writer
 import cv2
 from src.visualizer import Visualizer
 from src.video_loader import VideoLoader
 from src.video_writer import VideoWriter
 from src.detector import PersonDetector
+from src.tracker import PersonTracker
 
 
 class VideoProcessor:
 
     def __init__(self):
         self.detector = PersonDetector()
+        self.tracker = PersonTracker()
 
     def process(
         self,
@@ -37,9 +40,13 @@ class VideoProcessor:
             if not ret:
                 break
 
+
             detections = self.detector.detect(frame)
 
-            Visualizer.draw_detections(frame, detections)
+            tracked = self.tracker.update(detections)
+
+            Visualizer.draw_tracks(frame, tracked)
+
 
             writer.write(frame)
 
--- src/visualizer.py
+++ src/visualizer.py
@@ -37,3 +37,36 @@ class Visualizer:
             )
 
         return frame
+
+
+    @staticmethod
+    def draw_detections(frame, detections):
+        """
+        Draw person detections on a frame.
+        """
+
+        for det in detections:
+
+            x1, y1, x2, y2 = map(int, det.bbox)
+
+            cv2.rectangle(
+                frame,
+                (x1, y1),
+                (x2, y2),
+                (0, 255, 0),
+                2,
+            )
+
+            label = f"{det.confidence:.2f}"
+
+            cv2.putText(
+                frame,
+                label,
+                (x1, max(20, y1 - 10)),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                0.6,
+                (0, 255, 0),
+                2,
+            )
+
+        return frame
