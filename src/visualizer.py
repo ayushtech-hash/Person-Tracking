cfe62dabf065946f7cc2c3438a1de3887bb1dc3d
yash-odoo <yash.tis420@gmail.com>
2026-08-04 16:58:05 +0530

    update visualizer.py

--- src/visualizer.py
+++ src/visualizer.py
@@ -40,12 +40,12 @@ class Visualizer:
 
 
     @staticmethod
-    def draw_tracks(frame, detections):
+    def draw_tracks(frame, tracked_detections):
         """
         Draw person detections on a frame.
         """
 
-        for det in detections:
+        for det in tracked_detections:
 
             x1, y1, x2, y2 = map(int, det.bbox)
 
@@ -57,14 +57,15 @@ class Visualizer:
                 2,
             )
 
-            label = f"{det.confidence:.2f}"
+            label = f"ID {det.track_id}"
+
 
             cv2.putText(
                 frame,
                 label,
                 (x1, max(20, y1 - 10)),
                 cv2.FONT_HERSHEY_SIMPLEX,
-                0.6,
+                0.7,
                 (0, 255, 0),
                 2,
             )
